c2cb26504448beed16f5ee2efa556544a71aa612
mkesp <mariekat.espiritu@gmail.com>
2022-05-22 19:26:54 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## We have two functions that will allow to cache (temporarily store)
+## the date and solve for the inverse of the matrix if it isn't
+## already solved prior.
 
-## Write a short comment describing this function
+## makeCacheMatrix allows us to set and retrieve values for the matrix in an
+## environment. This is the function that cacheSolve will be
+## depending on.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i<-NULL
+  set<-function(y){
+    x<<-y
+    i<<-NULL
+  }
+  get<-function() x
+  setinv<-function(inv) i<<-inv
+  getinv<-function() i
+  list(set=set,
+       get=get,
+       setinv=setinv,
+       getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is responsible for retrieving the inverse value
+## of the matrix, and if it isn't existing yet, it solves for it
+## and then sets the value so that we may later retrieve it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  i<-x$getinv()
+  if(!is.null(i)){
+    message("getting cached data...")
+    return(i)
+  }
+  mtrx<-x$get()
+  i<-solve(mtrx, ...)
+  x$setinv(i)
+  i
 }
+
